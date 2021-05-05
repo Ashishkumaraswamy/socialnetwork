00e918262b72fc2fd13693af22b6272a4cc94baa
Ashish <ashishkumaraswamy@gmail.com>
2021-05-05 12:10:04 +0530

    signup initial stage

--- signup.php
+++ signup.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Login</title>
+	<title>signup</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->
@@ -11,14 +11,13 @@
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="css/animate.css">
 
 	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="css/util.css">
-	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="css/signup.css">
 <!--===============================================================================================-->
 <meta name="robots" content="noindex, follow">
 
@@ -40,8 +39,8 @@
   bottom: 0;
   left: 0;
   right: 0;
-  height: 70%;
-  width: 110%;
+  height: 50%;
+  width: 90%;
   opacity: 0;
   transition: .5s ease;
   background-color: #aa0468;
@@ -67,23 +66,26 @@
 <body>
 
 	<div class="limiter">
-		<div class="container-login100">
-			<div class="wrap-login100">
-			<div class="container">
-				<div class="login100-pic js-tilt" data-tilt>
-					<img src="images/logo1.png" alt="IMG">
-
- 						<div class="overlay">
-    					<div class="text">TREAT YOUR SELF!!</div>
-  						</div>
-					</div>
-				</div>
-
-				<form class="login100-form validate-form">
-					<span class="login100-form-title p-b-70" style="font-family: Georgia, serif; font-weight: bold; font-size: 50px">
-						LOG IN
+		<div class="container-signup100">
+			<div class="wrap-signup100">
+				<form class="signup100-form validate-form">
+					<span class="signup100-form-title p-b-70" style="font-family: Georgia, serif; font-weight: bold; font-size: 30px; text-align: center">
+						SIGN UP
 					</span>
-
+					<div class="name-details" style="display: flex">
+						<div class ="wrap-input100">
+							<div class="field input">
+							<input class ="input100" type="text" name="firstname" placeholder="First Name"/>
+							<span class="focus-input100"></span>	
+							</div>
+						</div>
+						<div class="wrap-input100">
+							<div class="field input">
+							<input class="input100" type="text" name="lastname" placeholder="Last Name"/>
+							<span class="focus-input100"></span>
+							</div>
+						</div>
+					</div>
 					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
 						<input class="input100" type="text" name="email" placeholder="Email">
 						<span class="focus-input100"></span>
@@ -100,25 +102,16 @@
 						</span>
 					</div>
 
-					<div class="container-login100-form-btn">
-						<button class="login100-form-btn">
-							Login
+					<div class="container-signup100-form-btn">
+						<button class="signup100-form-btn">
+							SIGN UP
 						</button>
 					</div>
 
-					<div class="text-center p-t-12">
-						<span class="txt1">
-							Forgot
-						</span>
-						<a class="txt2" href="#" style="text-decoration: none">
-							&nbsp Username / Password?
-						</a>
-					</div>
-
 					<div class="text-center p-t-20 p-b-70">
-						<p>Do not have an account?
-						<a class="txt2" href="signup.php">
-							Sign Up
+						<p>Already have an account?
+						<a class="txt2" href="index.php">
+							Log in
 							<i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
 						</a>
 						</p>
