e4058f962f1ffb5e366bf6805ba8223d3bc2aac9
mathanamathav <62739618+mathanamathav@users.noreply.github.com>
2021-05-07 16:46:04 +0530

    commit new page

---
+++ mainpage.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="css/mainpage.css">
+    <script src="http://fonts.cdnfonts.com/css/billabong" crossorigin='anonymous'></script>
+
+</head>
+  <body>
+  <div class="container-box">
+    <header class="header"></header>
+    <main class="main">
+      <section class="menu">  
+
+    <div class="navigation">
+            <div class="logo">
+            Social Media
+            </div>
+
+        <div class="navigation-search-container">
+            <i class="fa fa-search">
+            </i>
+            <input class="search-field" type="text" placeholder="Search">
+            <div class="search-container">
+            <div class="search-container-box">
+                <div class="search-results">
+
+                </div>
+            </div>
+            </div>
+        </div>
+
+        <div class="navigation-icons">
+            <a class="navigation-link">
+                <i class="far fa-compass"></i>
+            </a>
+            <a class="navigation-link notifica">
+            <i class="far fa-heart">
+            <div class="notification-bubble-wrapper">
+                <div class="notification-bubble">
+                    <span class="notifications-count">99</span>
+                </div>
+            </div>
+            </i></a>
+            
+            <a class="navigation-link">
+            <i class="far fa-user-circle"></i>
+            </a>
+            
+            <a id="signout" class="navigation-link">
+            <i class="fas fa-sign-out-alt"></i>
+            </a>
+        </div>
+    </div>  
+            
+      </section>
+      <section class="feed"></section>
+      <section class="event"></section>
+    </main>
+    <aside class="aside"></aside>
+  </div>
+</body>
+</html>
