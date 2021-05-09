b291860002b8cce9e66310279cfd7abfdb7ba397
Ashish <ashishkumaraswamy@gmail.com>
2021-05-09 18:16:43 +0530

    signup2 added

--- signup2.php
+++ signup2.php
@@ -10,10 +10,12 @@
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 	<script src='https://kit.fontawesome.com/a076d05399.js'></script>
+	<script type="text/javascript" src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
 
 <meta name="robots" content="noindex, follow">
 
 <style>
+
  .box {
   background: white;
   margin: auto;
@@ -23,6 +25,13 @@
   transition: all 0.3s cubic-bezier(.25,.8,.25,1);
 }
 
+.row
+{
+	width:300px;
+	height:300px;
+	align-items: center;
+	vertical-align: center;
+}
 .box:hover {
   border-top-left-radius: 10px;
   border-bottom-left-radius: 10px;
@@ -32,19 +41,53 @@
   box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
 }
 
+.profile-pic {
+    max-width: 200%;
+    max-height: 200px;
+    display: block;
+}
+
+.file-upload {
+    display: none;
+}
+.circle {
+	top:450px;
+	margin-left: 50px;
+	padding-bottom: 50px;
+    border-radius: 1000px !important;
+    overflow: hidden;
+    width: 200px;
+    height: 200px;
+    border: 8px solid rgba(0, 0, 0, 0.7);
+}
+
+.p-image {
+  margin-top:80px;
+  margin-left: 220px;
+  color: #666666;
+  transition: all .3s cubic-bezier(.175, .885, .32, 1.275);
+}
+.p-image:hover {
+  transition: all .3s cubic-bezier(.175, .885, .32, 1.275);
+}
+.upload-button {
+  font-size: 1.2em;
+}
+
+.upload-button:hover {
+  transition: all .3s cubic-bezier(.175, .885, .32, 1.275);
+  color: #999;
+}
 @keyframes example {
     0%   {border-left: 2px solid #ffffff;}
     25%  {border-left: 3px solid #ffe6e6;}
     50%  {border-left: 4px solid #ff8080;}
     100% {border-left: 5px solid #ff0000;}
 }
-  }
+}
 </style>
 </head>
 <body>
-
-	
-
 	<div class="limiter">
 		<div class="container-signup100">
 			
@@ -66,70 +109,21 @@
    						 </div>
 
 				      	<br>
-						<div class="name-details" style="display: flex">
-							<div class="wrap-input100 validate-input" data-validate = "Valid name is required: mathan">
-								<input class="input100" type="text" name="firstname" placeholder="First name">
-								<span class="focus-input100"></span>
-								<span class="symbol-input100">
-									<i class='fa fa-user-alt' style='font-size:17px'></i>
-								</span>
-							</div>
-							<p>&nbsp&nbsp&nbsp</p>
-
-							<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
-								<input class="input100" type="text" name="lastname" placeholder="Last name">
-								<span class="focus-input100"></span>
-								<span class="symbol-input100">
-									<i class='fa fa-user-alt' style='font-size:17px'></i>
-								</span>
-							</div>
-						</div>	
-
-						<div class="name-details" style="display: flex">
-							<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
-								<input class="input100" type="text" name="email" placeholder="Email">
-								<span class="focus-input100"></span>
-								<span class="symbol-input100">
-									<i class="fa fa-envelope" aria-hidden="true"></i>
-								</span>
-							</div>
-							<p>&nbsp&nbsp&nbsp</p>
-
-							<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
-								<input placeholder="Date of Birth" class="input100" type="text" onfocus="(this.type='date')" id="date">
-								<span class="focus-input100"></span>
-								<span class="symbol-input100">
-									<i class='fa fa-calendar-alt' style='font-size:17px'></i>
-								</span>
-							</div>
-						</div>	
-
-						<div class="name-details" style="display: flex">
-							<div class="wrap-input100 validate-input" data-validate = "Password is required">
-								<input class="input100" type="password" name="pass" placeholder="Password">
-								<span class="focus-input100"></span>
-								<span class="symbol-input100">
-									<i class="fa fa-lock" aria-hidden="true"></i>
-								</span>
-							</div>
-							<p>&nbsp&nbsp&nbsp</p>
-							<div class="wrap-input100 validate-input" data-validate = "Password is required">
-								<input class="input100" type="password" name="pass" placeholder="Confirm Password">
-								<span class="focus-input100"></span>
-								<span class="symbol-input100">
-									<i class="fa fa-lock" aria-hidden="true"></i>
-								</span>
-							</div>
-						</div>
-						<div class="container-signup100-form-btn">
-							<button class="signup100-form-btn">
-								<a href="index.php">COMPLETE</a>
-							</button>
+						<div class="row">
+						   <div class="small-12 medium-2 large-2 columns">
+						     <div class="circle">
+						       <img class="profile-pic" src="images/userlogo.PNG">
+						     </div>
+						     <div class="p-image">
+						       <i class="fa fa-camera upload-button"></i>
+						        <input class="file-upload" type="file" accept="image/*"/>
+						     </div>
+						  </div>
 						</div>
 					</form>
 				</div>
 		</div>
-
 	</div>
+	<script src="javascript/signup2.js"></script>
 </body>
 </html>
