eed3f597166d1e0bebcdbe0b319e327a62015c00
mathanamathav <62739618+mathanamathav@users.noreply.github.com>
2021-05-09 08:26:33 +0530

    icon updated!

--- signup2.php
+++ signup2.php
@@ -6,7 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="css/util.css">
 	<link rel="stylesheet" type="text/css" href="css/signup.css">
-	<link rel="icon" type="image/png" href="images/logoicon.ICO"/>
+	<link rel="icon" type="image/png" href="images/logoicon.ico"/>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 	<script src='https://kit.fontawesome.com/a076d05399.js'></script>
