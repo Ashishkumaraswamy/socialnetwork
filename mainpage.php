89b8e68a40ebd720b6f9550cd79977c674457073
mathanamathav <62739618+mathanamathav@users.noreply.github.com>
2021-05-09 16:45:01 +0530

    mainpage

--- mainpage.php
+++ mainpage.php
@@ -4,61 +4,48 @@
     <title>Home</title>
     <link rel="stylesheet" type="text/css" href="css/mainpage.css">
     <link rel="icon" type="image/png" href="images/logoicon.ico"/>
-    <script src="http://fonts.cdnfonts.com/css/billabong" crossorigin='anonymous'></script>
+    <script src="http://fonts.cdnfonts.com/css/billabong"></script>
 
 </head>
-  <body>
-  <div class="container-box">
-    <header class="header"></header>
-    <main class="main">
-      <section class="menu">  
-
-    <div class="navigation">
-            <div class="logo">
-            Social Media
-            </div>
-
-        <div class="navigation-search-container">
-            <i class="fa fa-search">
-            </i>
-            <input class="search-field" type="text" placeholder="Search">
-            <div class="search-container">
-            <div class="search-container-box">
-                <div class="search-results">
-
-                </div>
-            </div>
-            </div>
-        </div>
-
-        <div class="navigation-icons">
-            <a class="navigation-link">
-                <i class="far fa-compass"></i>
-            </a>
-            <a class="navigation-link notifica">
-            <i class="far fa-heart">
-            <div class="notification-bubble-wrapper">
-                <div class="notification-bubble">
-                    <span class="notifications-count">99</span>
-                </div>
-            </div>
-            </i></a>
-            
-            <a class="navigation-link">
-            <i class="far fa-user-circle"></i>
-            </a>
-            
-            <a id="signout" class="navigation-link">
-            <i class="fas fa-sign-out-alt"></i>
-            </a>
-        </div>
-    </div>  
-            
-      </section>
-      <section class="feed"></section>
-      <section class="event"></section>
-    </main>
-    <aside class="aside"></aside>
+<body>
+<div class="navigation">
+  <div class="logo">
+    <a class="no-underline" href="#">
+      logo here
+    </a>
   </div>
+  <div class="navigation-search-container">
+    <i class="fa fa-search"></i>
+    <input class="search-field" type="text" placeholder="Search">
+    <div class="search-container">
+      <div class="search-container-box">
+        <div class="search-results">
+
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="navigation-icons">
+    <a href="https://instagram.com/mimoudix" class="navigation-link">
+      <i class="far fa-compass"></i>
+    </a>
+    <a class="navigation-link notifica">
+      <i class="far fa-heart">
+        <div class="notification-bubble-wrapper">
+          <div class="notification-bubble">
+            <span class="notifications-count">99</span>
+          </div>
+        </div>
+      </i>
+    </a>
+    <a href="https://instagram.com/mimoudix" class="navigation-link">
+      <i class="far fa-user-circle"></i>
+    </a>
+    <a href="https://instagram.com/mimoudix" id="signout" class="navigation-link">
+      <i class="fas fa-sign-out-alt"></i>
+    </a>
+  </div>
+</div>
+  
 </body>
 </html>
