e2c37b06a37b4a0c96cab9ae1482a7a70f57a099
mathanamathav <62739618+mathanamathav@users.noreply.github.com>
2021-05-16 18:04:51 +0530

    ashish

--- php/data.php
+++ php/data.php
@@ -1,4 +1,4 @@
-<!-- <?php
+<?php
     $output .='<div style="width:500px; border:2px solid white; border-width:3px;background-color:white; border-radius:5px;">';
     while($row = mysqli_fetch_assoc($query)){
         $output .= '<a href="user.php?user_id='. $row['user_id'] .'" style="text-decoration: none;">
@@ -11,4 +11,4 @@
                 </a><hr>';
     }
     $output .='</div>';
-?> -->
+?> 
